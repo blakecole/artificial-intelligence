580ca0b7938411638e923f0209bcffcfaedb810d
Blake Cole <BLERK@MIT.EDU>
2019-11-03 20:56:32 -0500

    lab7: parts 1-6 complete. submitted (76/76 correct).

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,114 +1,222 @@
+# ********************************************************** #
+#    NAME: Blake Cole                                        #
+#    ORGN: MIT                                               #
+#    FILE: lab7.py                                           #
+#    DATE: 31 OCT 2019                                       #
+# ********************************************************** #
+
 # MIT 6.034 Lab 7: Support Vector Machines
 # Written by 6.034 staff
 
 from svm_data import *
 from functools import reduce
 
+INF = float('inf')
 
-#### Part 1: Vector Math #######################################################
+# Part 1: Vector Math #######################################################
+
 
 def dot_product(u, v):
-    """Computes the dot product of two vectors u and v, each represented 
+    """
+    Computes the dot product of two vectors u and v, each represented
     as a tuple or list of coordinates. Assume the two vectors are the
-    same length."""
-    raise NotImplementedError
+    same length.
+    """
+    if (len(u) != len(v)):
+        raise ValueError('ERROR: u and v  must be equal-length vectors)')
+    return(sum([(ui * vi) for ui, vi in zip(u, v)]))
+
 
 def norm(v):
-    """Computes the norm (length) of a vector v, represented 
-    as a tuple or list of coords."""
-    raise NotImplementedError
+    """
+    Computes the norm (length) of a vector v, represented
+    as a tuple or list of coords.
+    """
+    return(dot_product(v, v)**0.5)
 
 
-#### Part 2: Using the SVM Boundary Equations ##################################
+# Part 2: Using the SVM Boundary Equations ##################################
 
 def positiveness(svm, point):
-    """Computes the expression (w dot x + b) for the given Point x."""
-    raise NotImplementedError
+    """ Computes the expression (w dot x + b) for the given Point x. """
+    w = svm.w
+    b = svm.b
+    x = point.coords
+    return(dot_product(w, x) + b)
+
 
 def classify(svm, point):
-    """Uses the given SVM to classify a Point. Assume that the point's true
-    classification is unknown.
-    Returns +1 or -1, or 0 if point is on boundary."""
-    raise NotImplementedError
+    """
+    Uses given SVM to classify Point. Assume the point's true classification
+    is unknown. Returns +1 or -1, or 0 if point is on boundary.
+    """
+    point_value = positiveness(svm, point)
+
+    if (point_value > 0):
+        return(+1)
+    elif (point_value < 0):
+        return(-1)
+    else:
+        return(0)
+
 
 def margin_width(svm):
-    """Calculate margin width based on the current boundary."""
-    raise NotImplementedError
+    """ Calculate margin width based on the current boundary. """
+    width = 2 / norm(svm.w)
+    return(width)
+
 
 def check_gutter_constraint(svm):
-    """Returns the set of training points that violate one or both conditions:
-        * gutter constraint (positiveness == classification, for support vectors)
+    """
+    Returns the set of training points that violate one or both conditions:
+        * gutter constraint (positiveness == class, for support vectors)
         * training points must not be between the gutters
-    Assumes that the SVM has support vectors assigned."""
-    raise NotImplementedError
+    Assumes that the SVM has support vectors assigned.
+    """
+    bad_points = set()
+
+    for point in svm.training_points:
+        point_value = positiveness(svm, point)
+
+        if ((point_value > -1) and (point_value < 1)):
+            bad_points.add(point)
+
+        if (point in svm.support_vectors):
+            if ((point.classification == +1 and point_value != +1)
+                    or (point.classification == -1 and point_value != -1)):
+                bad_points.add(point)
+
+    return(bad_points)
 
 
-#### Part 3: Supportiveness ####################################################
+# Part 3: Supportiveness ####################################################
 
 def check_alpha_signs(svm):
-    """Returns the set of training points that violate either condition:
+    """
+    Returns the set of training points that violate either condition:
         * all non-support-vector training points have alpha = 0
         * all support vectors have alpha > 0
     Assumes that the SVM has support vectors assigned, and that all training
-    points have alpha values assigned."""
-    raise NotImplementedError
+    points have alpha values assigned.
+    """
+    bad_points = set()
+
+    for point in svm.training_points:
+        alpha = point.alpha
+        if (point in svm.support_vectors):
+            if (alpha <= 0):
+                bad_points.add(point)
+        else:
+            if (alpha != 0):
+                bad_points.add(point)
+
+    return(bad_points)
+
 
 def check_alpha_equations(svm):
-    """Returns True if both Lagrange-multiplier equations are satisfied,
+    """
+    Returns True if both Lagrange-multiplier equations are satisfied,
     otherwise False. Assumes that the SVM has support vectors assigned, and
-    that all training points have alpha values assigned."""
-    raise NotImplementedError
+    that all training points have alpha values assigned.
+    """
+    ya_sum = 0
+    yax_sum = [0] * len(svm.w)
+
+    for point in svm.training_points:
+        yalpha = point.classification * point.alpha
+        ya_sum += yalpha
+        yax_sum = vector_add(yax_sum, scalar_mult(yalpha, point.coords))
+
+    if (ya_sum == 0 and svm.w == yax_sum):
+        return(True)
+    else:
+        return(False)
 
 
-#### Part 4: Evaluating Accuracy ###############################################
+# Part 4: Evaluating Accuracy ###############################################
 
 def misclassified_training_points(svm):
-    """Returns the set of training points that are classified incorrectly
-    using the current decision boundary."""
-    raise NotImplementedError
+    """
+    Returns the set of training points that are classified incorrectly
+    using the current decision boundary.
+    """
+    misclassified = set()
+
+    for point in svm.training_points:
+        if (point.classification != classify(svm, point)):
+            misclassified.add(point)
+
+    return(misclassified)
 
 
-#### Part 5: Training an SVM ###################################################
+# Part 5: Training an SVM ###################################################
 
 def update_svm_from_alphas(svm):
-    """Given an SVM with training data and alpha values, use alpha values to
-    update the SVM's support vectors, w, and b. Return the updated SVM."""
-    raise NotImplementedError
+    """
+    Given an SVM with training data and alpha values, use alpha values to
+    update the SVM's support vectors, w, and b. Return the updated SVM.
+    """
+    # Reset support vector list, initialize sum(y*alpha*x):
+    svm.support_vectors.clear()
+    yax_sum = [0, 0]
+
+    # Update w, support_vectors:
+    for point in svm.training_points:
+        yalpha = point.classification * point.alpha
+        yax_sum = vector_add(yax_sum, scalar_mult(yalpha, point.coords))
+        if (point.alpha > 0):
+            svm.support_vectors.append(point)
+    svm.w = yax_sum
+
+    # Update b:
+    b_min = INF
+    b_max = -INF
+
+    for sv in svm.support_vectors:
+        y = sv.classification
+        b = y - dot_product(svm.w, sv.coords)
+
+        if ((y == -1) and (b < b_min)):
+            b_min = b
+        elif ((y == +1) and (b > b_max)):
+            b_max = b
+
+    svm.b = (b_min + b_max)/2
+
+    return(svm)
 
 
-#### Part 6: Multiple Choice ###################################################
+# Part 6: Multiple Choice ###################################################
+ANSWER_1 = 11
+ANSWER_2 = 6
+ANSWER_3 = 3
+ANSWER_4 = 2
 
-ANSWER_1 = None
-ANSWER_2 = None
-ANSWER_3 = None
-ANSWER_4 = None
+ANSWER_5 = ['A', 'D']
+ANSWER_6 = ['A', 'B', 'D']
+ANSWER_7 = ['A', 'B', 'D']
+ANSWER_8 = []
+ANSWER_9 = ['A', 'B', 'D']
+ANSWER_10 = ['A', 'B', 'D']
 
-ANSWER_5 = None
-ANSWER_6 = None
-ANSWER_7 = None
-ANSWER_8 = None
-ANSWER_9 = None
-ANSWER_10 = None
+ANSWER_11 = False
+ANSWER_12 = True
+ANSWER_13 = False
+ANSWER_14 = False
+ANSWER_15 = False
+ANSWER_16 = True
 
-ANSWER_11 = None
-ANSWER_12 = None
-ANSWER_13 = None
-ANSWER_14 = None
-ANSWER_15 = None
-ANSWER_16 = None
+ANSWER_17 = [1, 3, 6, 8]
+ANSWER_18 = [1, 2, 4, 5, 6, 7, 8]
+ANSWER_19 = [1, 2, 4, 5, 6, 7, 8]
 
-ANSWER_17 = None
-ANSWER_18 = None
-ANSWER_19 = None
+ANSWER_20 = 6
 
-ANSWER_20 = None
+# SURVEY ####################################################################
 
-
-#### SURVEY ####################################################################
-
-NAME = None
-COLLABORATORS = None
-HOW_MANY_HOURS_THIS_LAB_TOOK = None
-WHAT_I_FOUND_INTERESTING = None
-WHAT_I_FOUND_BORING = None
-SUGGESTIONS = None
+NAME = 'Blake Cole'
+COLLABORATORS = ''
+HOW_MANY_HOURS_THIS_LAB_TOOK = 6
+WHAT_I_FOUND_INTERESTING = 'SVMs are cool!  I feel like there is so much moreto explore, and that we only just scratched the surface.  This thought is exciting, inticing, and slightly intimidating.'
+WHAT_I_FOUND_BORING = 'Some of the required programs were a bit mundane.  But, I was also glad to finish the lab reasonably quickly for once.'
+SUGGESTIONS = 'N/A'
--- lab7/train_svm.py
+++ lab7/train_svm.py
@@ -3,11 +3,11 @@
 # MIT 6.034 Lab 7: Support Vector Machines
 # This file is based on code originally written by Crystal Pan.
 
+from time import time
 from lab7 import *
 from display_svm import create_svm_graph
 from functools import reduce
 INF = float('inf')
-from time import time
 
 
 """
@@ -38,13 +38,13 @@ def train_svm(training_points, kernel_fn=dot_product, max_iter=500,
     """Performs SMO using all of the training points until there are no changed
     alphas or until the max iteration depth is reached"""
     # Define alias for kernel function, converting Points to coordinates just in case
-    K = lambda p1, p2: kernel_fn(p1.coords, p2.coords)
+    def K(p1, p2): return kernel_fn(p1.coords, p2.coords)
 
     # Copy training data to avoid modifying input data
     training_points = deepcopy(training_points)
 
     # Initialize SVM
-    svm = SupportVectorMachine([0,0], 0, training_points, [])
+    svm = SupportVectorMachine([0, 0], 0, training_points, [])
 
     if show_graph:
         if not manual_animation:
@@ -53,7 +53,7 @@ def train_svm(training_points, kernel_fn=dot_product, max_iter=500,
         if (animate and animation_delay > 0) or manual_animation:
             # Keep track of last update time and old SVM values
             last_update_time = time()
-            old_w = [0,0]
+            old_w = [0, 0]
             old_b = 0
             old_support_vectors = []
 
@@ -86,16 +86,20 @@ def train_svm(training_points, kernel_fn=dot_product, max_iter=500,
                     upper_bound = INF
 
                 # Compute current error of alpha_i and alpha_j
-                error_i = reduce(lambda total,pt: total + (pt.classification * pt.alpha * K(i,pt) + b), training_points, 0) - i.classification
-                error_j = reduce(lambda total,pt: total + (pt.classification * pt.alpha * K(j,pt) + b), training_points, 0) - j.classification
+                error_i = reduce(lambda total, pt: total + (pt.classification *
+                                                            pt.alpha * K(i, pt) + b), training_points, 0) - i.classification
+                error_j = reduce(lambda total, pt: total + (pt.classification *
+                                                            pt.alpha * K(j, pt) + b), training_points, 0) - j.classification
 
                 # Store old alpha values before updating
                 old_alpha_i = i.alpha
                 old_alpha_j = j.alpha
 
                 # Update j.alpha, but keep it between lower and upper bounds
-                n = 2 * K(i,j) - K(i,i) - K(j,j) # Note: if K is dot_product, n = -||i-j||^2
-                j.alpha = old_alpha_j - ( j.classification * (error_i - error_j) / float(n) )
+                # Note: if K is dot_product, n = -||i-j||^2
+                n = 2 * K(i, j) - K(i, i) - K(j, j)
+                j.alpha = old_alpha_j - \
+                    (j.classification * (error_i - error_j) / float(n))
                 if j.alpha > upper_bound:
                     j.alpha = upper_bound
                 elif j.alpha < lower_bound:
@@ -106,10 +110,12 @@ def train_svm(training_points, kernel_fn=dot_product, max_iter=500,
                     continue
 
                 # Update i.alpha, but ensure it stays non-negative
-                i.alpha = max(0, old_alpha_i + (i.classification * j.classification) * (old_alpha_j - j.alpha))
+                i.alpha = max(0, old_alpha_i + (i.classification *
+                                                j.classification) * (old_alpha_j - j.alpha))
 
                 # Update SVM based on alphas
-                update_svm_from_alphas(svm) # Note: kernel_fn is hardcoded as dot_product
+                # Note: kernel_fn is hardcoded as dot_product
+                update_svm_from_alphas(svm)
 
                 # Update b from SVM
                 b = svm.b
@@ -119,8 +125,8 @@ def train_svm(training_points, kernel_fn=dot_product, max_iter=500,
                 if animation_delay > 0 or manual_animation:
                     # If values have not changed perceptibly, skip this update
                     if ([sv.name for sv in svm.support_vectors] == [sv.name for sv in old_support_vectors]
-                          and ((b==0 and old_b==0 and list_approx_equal(svm.w, old_w, 0.001))
-                               or (b!=0 and old_b !=0 and list_approx_equal(scalar_mult(1./b, svm.w), scalar_mult(1./old_b, old_w), 0.001)))):
+                        and ((b == 0 and old_b == 0 and list_approx_equal(svm.w, old_w, 0.001))
+                             or (b != 0 and old_b != 0 and list_approx_equal(scalar_mult(1./b, svm.w), scalar_mult(1./old_b, old_w), 0.001)))):
                         show_update = False
                     else:
                         # Update old stored values
@@ -152,12 +158,12 @@ def train_svm(training_points, kernel_fn=dot_product, max_iter=500,
     print('# iterations:', iteration)
 
     # Compute final w, b, and SVs based on alphas
-    update_svm_from_alphas(svm) # Note: kernel_fn is hardcoded as dot_product
+    update_svm_from_alphas(svm)  # Note: kernel_fn is hardcoded as dot_product
 
     # Check training
     misclassified = misclassified_training_points(svm)
-    print("Training complete! SVM with decision boundary %.3f*x + %.3f*y + %.3f >= 0 misclassified %i points." \
-        % (svm.w[0], svm.w[1], svm.b, len(misclassified)))
+    print("Training complete! SVM with decision boundary %.3f*x + %.3f*y + %.3f >= 0 misclassified %i points."
+          % (svm.w[0], svm.w[1], svm.b, len(misclassified)))
 
     if show_graph:
         if manual_animation:
@@ -171,8 +177,8 @@ def train_svm(training_points, kernel_fn=dot_product, max_iter=500,
 
 if __name__ == '__main__':
     # Uncomment below to train on a different dataset.  Feel free to use different arguments!
-    train_svm(sample_data_1)
-#    train_svm(sample_data_2)
-#    train_svm(recit_data)
-#    train_svm(harvard_mit_data)
-#    train_svm(unseparable_data, animation_delay=0)
+    # train_svm(sample_data_1)
+    # train_svm(sample_data_2)
+    # train_svm(recit_data)
+    train_svm(harvard_mit_data)
+    # train_svm(unseparable_data, animation_delay=0)
